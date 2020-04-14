56c531ba6767ffd4ccf030644d9d92e20fd3c431
Adrian <adrian_zinko@op.pl>
2020-04-14 08:05:07 +0200

    feat: config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path');
+const HtmlWebpackPlugin = require ('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
-  }
+  }, 
+  mode: 'development',
+  plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      })
+    ]
 };
