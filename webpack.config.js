776c37b76b0b070e6818dc70d5e9ab709b110392
Adrian <adrian_zinko@op.pl>
2020-04-14 23:24:17 +0200

    feat: config style loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,19 @@ module.exports = {
     filename: 'bundle.js'
   }, 
   mode: 'development',
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+
   plugins: [
       new HtmlWebpackPlugin({
         template: './src/index.html',
       })
-    ]
+    ],
 };
