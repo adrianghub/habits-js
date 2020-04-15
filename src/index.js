123dea347bcb3fc0984a83c0b180b3e6850804d2
Adrian <adrian_zinko@op.pl>
2020-04-15 21:42:12 +0200

    feat: set countdown

--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ import './style.css';
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
 
+let countdown;
+
 function timer(seconds) {
     now = Date.now();
     then = now + (seconds * 1000);
 }
+
+countdown = setInterval(() => {
+    let secondsLeft = Math.round((then - Date.now()) / 1000);
+
+    if(secondsLeft < 0) {
+        clearInterval(countdown);
+        return;
+    }
+})
