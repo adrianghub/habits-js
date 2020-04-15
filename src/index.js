54fa437adcc8633f277d4574ff28982906ee9aff
Adrian <adrian_zinko@op.pl>
2020-04-15 21:40:21 +0200

    feat: setup timer function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './style.css';
+
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+
+function timer(seconds) {
+    now = Date.now();
+    then = now + (seconds * 1000);
+}
